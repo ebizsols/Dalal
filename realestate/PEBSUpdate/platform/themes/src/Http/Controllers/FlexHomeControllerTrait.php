ca07cd15a905f4d4f2685b92ae60a8e5bdeee38a
Qasim Rafique <qasim.online.now@gmail.com>
2021-11-30 18:03:09 +0500

    sparating custom code

--- realestate/PEBSUpdate/platform/themes/src/Http/Controllers/FlexHomeControllerTrait.php
+++ realestate/PEBSUpdate/platform/themes/src/Http/Controllers/FlexHomeControllerTrait.php
@@ -1,7 +1,11 @@
 <?php
 
+use Botble\Agency\Models\Agency;
 use Botble\Agency\Repositories\Interfaces\AgencyInterface;
+use Botble\RealEstate\Models\Account;
+use Botble\RealEstate\Models\Property;
 use Botble\RealEstate\Repositories\Interfaces\AccountInterface;
+use Botble\RealEstate\Repositories\Interfaces\PropertyInterface;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
@@ -25,5 +29,69 @@ trait FlexHomeControllerTrait{
         Theme::breadcrumb()->add(__('Home'), route('public.index'))->add(__('Agencies'), route('public.agencies'));
 
         return Theme::scope('PEBSUpdate.real-estate.agency.agencies', compact('agencies'))->render();
+    }
+
+    public function getAgency(
+        string $id,
+        Request $request,
+        AgencyInterface $agencyRepository,
+        PropertyInterface $propertyRepository,
+        AccountInterface  $accountRepository
+    ) {
+        $agency = $agencyRepository->getFirstBy(['id' => $id]);
+
+        if (!$agency) {
+            //echo "hello"; exit;
+            abort(404);
+        }
+
+        SeoHelper::setTitle($agency->title);
+
+        $properties = $propertyRepository->advancedGet([
+            'condition' => [
+                'author_id'   => $agency->id,
+                'author_type' => Agency::class,
+            ],
+            'paginate'  => [
+                'per_page'      => 12,
+                'current_paged' => (int)$request->input('page'),
+            ],
+            'with'      => config('plugins.real-estate.real-estate.properties.relations'),
+        ]);
+
+
+        $AgencyAccountRefrence = new AgencyAccountRefrence();
+        $AgencyAccountRefrencelisting = $AgencyAccountRefrence->where('agency_id',$id)->get()->toArray();
+        $accountList = array();
+        foreach ($AgencyAccountRefrencelisting as $something){
+            $accountId = $something['account_id'];
+
+            $account = new Account();
+            $accountData = $account->where('id', $accountId)->first();
+            if(!empty($accountData)){
+                $accountData = $accountData->toArray();
+            }
+
+            $accountList[] = $accountData;
+
+        }
+
+        $agency = $agencyRepository->getFirstBy(['id' => $id]);
+        $AgencyAccountRefrence = new AgencyAccountRefrence();
+        $AgencyAccountRefrencelisting = $AgencyAccountRefrence->where('agency_id', $id)->get()->toArray();
+        $property = array();
+        foreach ($AgencyAccountRefrencelisting as $something) {
+            $accountId = $something['account_id'];
+
+            $account = new Property();
+            $accountData = $account->where('id', $accountId)->first();
+            if (!empty($accountData)) {
+                $accountData = $accountData->toArray();
+                $property[] = $accountData;
+            }
+        }
+
+        return Theme::scope('real-estate.agency', compact('properties','property', 'agency','accountList'))
+            ->render();
     }
 }
